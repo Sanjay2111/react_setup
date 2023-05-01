d6c5d464356ae59695678bbfd86a1c9fe056079b
Sanjay <sanjayluitel2@gmail.com>
2023-05-01 09:54:37 -0400

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,9 @@ module.exports = {
     project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
-  rules: { 'react/react-in-jsx-scope': 0 },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
 };
